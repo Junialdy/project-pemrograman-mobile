b413ede234f7a22cf2f50a156128699cf2311dc5
nadiatall <ntalitha0@gmail.com>
2024-12-13 09:05:22 +0700

    feat : prepare heatmap datasets (still errors in here)

--- lib/util/habit_util.dart
+++ lib/util/habit_util.dart
@@ -1,3 +1,5 @@
+import 'package:habittute/models/habit.dart';
+
 bool isHabitCompletedToday(List<DateTime> completedDays) {
   final today = DateTime.now();
   return completedDays.any(
@@ -6,4 +8,23 @@ bool isHabitCompletedToday(List<DateTime> completedDays) {
         date.month == today.month &&
         date.day == today.day,
   );
+}
+
+//prepare heatmap datasets
+Map<DataTime, int> prepareHeatMapDataset(List<Habit> habits) {
+  Map<DataTime, int> dataset = {};
+
+  for(var habit in habits) {
+    for(var date in habit.completedDays) {
+      final normalizedDate = DateTime(date.year, date.month, date.day);
+
+      if (dataset.containsKey(normalizedDate)){
+        dataset[normalizedDate] = dataset[normalizedDate]! + 1;
+      } else{
+        dataset[normalizedDate] = 1;
+      }
+    }
+  }
+
+ return dataset;
 }
